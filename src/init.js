d97cbf87ccf7eea4ee643a68a08049b20abfe5e7
Tracey Kirk <tkirk@c02v58dxhtd6.attlocal.net>
2019-02-18 21:12:49 -0600

    tkirk cep 2019 entry

--- src/init.js
+++ src/init.js
@@ -1,4 +1,48 @@
-// create a JavaScript object here with the following fields: firstName, lastName, jobTitle, homeOffice
+// declare variables to hold buttons and info
+var info;
+var buttons;
+var pToDisplay;
+window.onload=function(){
+  // When the page loads grab the buttons from index.html,
+  buttons = document.querySelectorAll(".button");
+  // grab the person's info and put in info;
+  info = document.querySelectorAll(".info");
+// hide the paragraphs containing the person's info
+//$(document).ready(() => {
+//  $('.info').hide()
+//});
 
+// attach event listener to buttons
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function() {
+      clickedButton=document.getElementById("")
+      displayInfo(this);
+    }
+  }
+}
+  // Show the info for the selected button when it is clicked
+  function displayInfo(infoToDisplay){
+    console.log("inside the function", infoToDisplay);
+  //  $('.button').on('click', () => {
+    //   $('.info').show();
+     //});
 
-// using jQuery and the object above, display the information as the appropriate button is clicked
+     var pToDisplay = document.getElementById("firstName p");
+     console.log(pToDisplay);
+if (pToDisplay.style.display === "none") {
+  pToDisplay.style.display = "block";
+} else {
+  pToDisplay.style.display = "none";
+}
+    //console.log(infoToDisplay);
+//   for (var i = 0; i < buttons.length; i++){
+//      if (buttons.length[i] == infoToDisplay){
+//        console.log("hi", buttons.length[i]);
+//        $(info[i]).show();
+//      }
+//      else {
+//        //info.classList.remove("active");
+
+  }
+//  }
+//}
